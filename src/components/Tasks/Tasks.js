a699cac80d92cd373e6e55d88d1d48dc2e31454a
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-04-12 00:19:03 +0300

    фикс чекбоксов в задачах

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -11,11 +11,11 @@ const Tasks = ({ list }) => {
         <img src={EditSvg} alt="Edit icon" />
       </h2>
       <div className="tasks__items">
-        {list.tasks.map((task, index) => (
-          <div className="tasks__items-row" key={index}>
+        {list.tasks.map((task) => (
+          <div className="tasks__items-row" key={task.id}>
             <div className="checkbox">
-              <input id="check" type="checkbox" />
-              <label htmlFor="check">
+              <input id={`task-${task.id}`} type="checkbox" />
+              <label htmlFor={`task-${task.id}`}>
                 <svg
                   width="11"
                   height="8"
@@ -33,7 +33,7 @@ const Tasks = ({ list }) => {
                 </svg>
               </label>
             </div>
-            <input value={task.text} />
+            <input readOnly value={task.text} />
           </div>
         ))}
       </div>
